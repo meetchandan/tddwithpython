e7db2e12187c551b4d231c02fad85cc8b1d3df3b
meetchandan <chandan00104@gmail.com>
2014-11-22 13:23:28 +0530

    putting tests in classes

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,8 +1,25 @@
 __author__ = 'cbhattad'
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://127.0.0.1:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+
+        self.fail('Finish the Test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
